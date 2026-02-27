223cdcbe26fd45cfc6d95ea55bc3a4246b9b5901
omkara472 <omkarmareedu472@gmail.com>
2026-02-27 12:48:29 +0530

    Add auto-generated tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,11 @@
 import pytest
 from src import app
 
-def test_driver():
-    result = app.driver()
+def test_test_add_returns_value():
+    result = app.test_add()
     assert result is not None
 
-def test_test_scrum_6_login():
-    result = app.test_scrum_6_login(None)
-    assert result is not None
 
+def test_test_subtract_returns_value():
+    result = app.test_subtract()
+    assert result is not None
