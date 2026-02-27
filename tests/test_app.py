a8938fc2804bf428695c5178050e929f777bc69f
omkara472 <omkarmareedu472@gmail.com>
2026-02-27 23:41:15 +0530

    Add tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,11 @@
 import pytest
 from src import app
 
-def test_driver():
+def test_driver_returns_value():
     result = app.driver()
     assert result is not None
 
-def test_test_scrum_6_login():
-    result = app.test_scrum_6_login(None)
-    assert result is not None
 
+def test_test_login_functionality_returns_value():
+    result = app.test_login_functionality(None)
+    assert result is not None
