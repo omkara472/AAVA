790525563ce68a7c363e46234d8fd3773345ec6d
omkara472 <omkarmareedu472@gmail.com>
2026-03-02 12:27:56 +0530

    Auto Bootstrap: CI/CD + Multi Test Files

--- src/app.py
+++ src/app.py
@@ -1,11 +1,1 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-
-def test_google_title():
-    driver = webdriver.Chrome()
-    driver.get("https://www.google.com")
-
-    assert "Google" in driver.title
-
-    driver.quit()
+apple
