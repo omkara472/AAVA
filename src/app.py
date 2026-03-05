d9d96cc1e678f8f44f789632397202f3aa271098
omkara472 <omkarmareedu472@gmail.com>
2026-03-05 13:39:58 +0530

    Update src/app.py

--- src/app.py
+++ src/app.py
@@ -1,1 +1,8 @@
-print('hello world')
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://demo.opencart.com")
+driver.find_element(By.NAME, "search").send_keys("MacBook")
+print(driver.title)
+driver.quit()
