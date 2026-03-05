6319e75c5548fec1888a9a58e465bb4c7ff9fa44
omkara472 <omkarmareedu472@gmail.com>
2026-03-05 13:07:58 +0530

    Update tests/test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,22 @@
+from src import app
 
-def test_placeholder():
-    assert True
+
+def test_driver():
+    result = app.driver()
+    assert result is not None
+
+def test_test_homepage_title():
+    result = app.test_homepage_title(None)
+    assert result is not None
+
+def test_test_search_product():
+    result = app.test_search_product(None)
+    assert result is not None
+
+def test_test_add_to_cart():
+    result = app.test_add_to_cart(None)
+    assert result is not None
+
+def test_test_cart_button():
+    result = app.test_cart_button(None)
+    assert result is not None
