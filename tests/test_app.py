63b4d7f3a595f35869468422b9094dc14ef32949
omkara472 <omkarmareedu472@gmail.com>
2026-03-16 11:46:53 +0530

    Update tests/test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,10 @@
+from src import app
 
-def test_placeholder():
-    assert True
+
+def test_driver():
+    result = app.driver()
+    assert result is not None
+
+def test_test_verify_login_functionality():
+    result = app.test_verify_login_functionality(None)
+    assert result is not None
